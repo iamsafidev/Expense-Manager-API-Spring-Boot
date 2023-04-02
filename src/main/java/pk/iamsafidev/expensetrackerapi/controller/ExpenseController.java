82bb29a60fd1e462deb62ee26271e735ffff352b
iamsafidev <safiawan099@gmail.com>
2023-04-02 09:50:09 +0500

    Exploring the Path Variable

--- src/main/java/pk/iamsafidev/expensetrackerapi/controller/ExpenseController.java
+++ src/main/java/pk/iamsafidev/expensetrackerapi/controller/ExpenseController.java
@@ -2,6 +2,7 @@ package pk.iamsafidev.expensetrackerapi.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import pk.iamsafidev.expensetrackerapi.entity.Expense;
 import pk.iamsafidev.expensetrackerapi.service.ExpenseService;
@@ -10,10 +11,16 @@ import java.util.List;
 
 @RestController
 public class ExpenseController {
-@Autowired
+    @Autowired
     private ExpenseService expenseService;
+
     @GetMapping("/expenses")
     public List<Expense> getAllExpenses() {
         return expenseService.getAllExpenses();
+    }
+
+    @GetMapping("/expenses/{id}")
+    public String getExpenseById(@PathVariable("id") Long id) {
+        return "expenseService.getAllExpenses().get(0);: " + id;
     }
 }
