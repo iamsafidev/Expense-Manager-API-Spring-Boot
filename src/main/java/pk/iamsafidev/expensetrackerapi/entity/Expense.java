be48fbd8cca72bc323af425a73665606c2c061f6
iamsafidev <safiawan099@gmail.com>
2023-04-05 00:08:21 +0500

    Add CreatedAt and updatedAt timeStamp in the expense Table using JPA

--- src/main/java/pk/iamsafidev/expensetrackerapi/entity/Expense.java
+++ src/main/java/pk/iamsafidev/expensetrackerapi/entity/Expense.java
@@ -4,27 +4,37 @@ import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
 import java.math.BigDecimal;
 import java.sql.Date;
+import java.sql.Timestamp;
 
 @Entity
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-@Table(name="tbl_expenses")
+@Table(name = "tbl_expenses")
 public class Expense {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name="expense_name")
+    @Column(name = "expense_name")
     private String name;
     private String description;
-    @Column(name="expense_amount")
+    @Column(name = "expense_amount")
     private BigDecimal amount;
     private String category;
     private Date date;
 
+    @Column(name = "created_at", nullable = false, updatable = false)
+    @CreationTimestamp
+    private Timestamp createdAt;
+
+    @Column(name = "updated_at")
+    @UpdateTimestamp
+    private Timestamp updatedAt;
 
 }
